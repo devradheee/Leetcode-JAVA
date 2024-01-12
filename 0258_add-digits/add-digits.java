a78e2f4cd85fede540a8b55d5dbb9fd9e4f4452c
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2024-01-12 14:58:30 +0000

    Day-12

---
+++ 0258_add-digits/add-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int addDigits(int num) {
+        int res = calc(num);
+        while (res > 9)
+            res = calc(res);
+
+        return res;
+    }
+
+    int calc(int num) {
+        if (num == 0)
+            return 0;
+        return num % 10 + calc(num / 10);
+    }
+}
