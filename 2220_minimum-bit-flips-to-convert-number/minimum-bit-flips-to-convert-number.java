1365aec71db53fc06a5e8b94274631b27919400c
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2024-02-01 16:37:37 +0000

    Day - 32

---
+++ 2220_minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,16 @@
+// package 2220_minimum-bit-flips-to-convert-number;
+
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int steps = 0;
+
+        while (start > 0 || goal > 0) {
+            if ((start & (1)) != (goal & (1))) {
+                steps++;
+            }
+            start = start >> 1;
+            goal = goal >> 1;
+        }
+        return steps;
+    }
+}
