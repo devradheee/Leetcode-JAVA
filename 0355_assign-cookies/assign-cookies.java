b3ac45d86c0087db479d11a28c30c30ed16c92e3
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2024-02-12 15:54:32 +0000

    Day - 42

---
+++ 0355_assign-cookies/assign-cookies.java
@@ -0,0 +1,26 @@
+// package 0355_assign-cookies;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int cookies_numbers = s.length;
+        if (cookies_numbers == 0)
+            return 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int maximum_number_count = 0;
+        int cookieIndex = cookies_numbers - 1;
+        int childIndex = g.length - 1;
+        while (cookieIndex >= 0 && childIndex >= 0) {
+            if (s[cookieIndex] >= g[childIndex]) {
+                maximum_number_count++;
+                cookieIndex--;
+                childIndex--;
+            } else {
+                childIndex--;
+            }
+        }
+
+        return maximum_number_count;
+    }
+}
