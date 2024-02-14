772c48801f1fceff44f335545df40a00eb68246d
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2024-02-14 17:53:03 +0000

    Day - 43

---
+++ 0055_jump-game/jump-game.java
@@ -0,0 +1,20 @@
+package 0055_jump-game;
+
+class Solution {
+    public boolean canJump(int[] arr) {
+        int target = arr.length - 1;
+
+        if (arr.length == 1)
+            return true;
+
+        int i = arr.length - 2;
+        while (i >= 0) {
+            if (arr[i] >= (target - i)) {
+                target = i;
+            }
+            i--;
+        }
+
+        return target == 0;
+    }
+}
