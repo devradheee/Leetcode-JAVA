b69ff154bd98a509368c6a3ccdae7e75e591390f
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2024-02-15 18:05:15 +0000

    Day - 44

---
+++ 0435_non-overlapping-intervals/non-overlapping-intervals.java
@@ -0,0 +1,17 @@
+public class non-overlapping-intervals {
+    public int eraseOverlapIntervals(int[][] intervals) {;
+        Arrays.sort(intervals,(a,b)->a[1]-b[1]);
+        Stack<int[]> stk = new Stack<>();
+        int res=0;
+        for(int[] i : intervals){
+            if(!stk.isEmpty() && i[0]< stk.peek()[1]){
+                res++;
+            }else{
+                stk.push(i);
+            }
+        }
+
+    return res;
+    }
+}
+}
